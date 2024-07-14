2231c5a43d6321bfd35ebe8c427cf8c70a4c3ccc
minju <amilk5u@naver.com>
2024-07-14 20:34:39 +0900

    add : 제네릭 파일 추가

--- 8_object/1_defining_object.ts
+++ 8_object/1_defining_object.ts
@@ -2,28 +2,28 @@
  * Object 
  */
 const codefactory = {
-    name: '코드팩토리',
-    age: 32,
+  name: '코드팩토리',
+  age: 32,
 }
 
-interface IPerson{
-    name: string;
-    age: number;
+interface IPerson {
+  name: string;
+  age: number;
 }
 
 type TPerson = {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
 const iPerson: IPerson = {
-    name: '아이유',
-    age: 30,
+  name: '아이유',
+  age: 30,
 }
 
 const tPerson: TPerson = {
-    name: '유인나',
-    age: 30,
+  name: '유인나',
+  age: 30,
 }
 
 iPerson.name;
