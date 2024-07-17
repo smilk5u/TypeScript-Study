a030c3753ccd5fe219a26d2498b9098b3d3e70f7
minju <amilk5u@naver.com>
2024-07-18 08:11:24 +0900

    abstart class

--- 9_class/8_abstract_class.ts
+++ 9_class/8_abstract_class.ts
@@ -2,28 +2,28 @@
  * Abstract class
  */
 abstract class ModelWithId {
-    id: number;
+  id: number;
 
-    constructor(id: number) {
-        this.id = id;
-    }
+  constructor(id: number) {
+    this.id = id;
+  }
 }
 
 // const modelWithId = new ModelWithId(123);
 
-class Product extends ModelWithId{
+class Product extends ModelWithId {
 
-}   
-
-const product = new Product(1);
-product.id;
-
-abstract class ModelWithAbstractMethod{
-    abstract shout(name: string): string;
 }
 
-class Person extends ModelWithAbstractMethod{
-    shout(name: string): string {
-        return '소리질러~';
-    }
+const product = new Product(1); 
+product.id;
+
+abstract class ModelWithAbstractMethod {
+  abstract shout(name: string): string;
+}
+
+class Person extends ModelWithAbstractMethod {
+  shout(name: string): string {
+    return '소리질러~';
+  }
 }
