faaed1816ae8f0c2fca616b1b29de3acdb4446f6
juju <55374275+smilk5u@users.noreply.github.com>
2024-07-22 20:17:11 +0900

    chore : 주석 추가

--- 12_experimental_decorator/2_method_decorator.ts
+++ 12_experimental_decorator/2_method_decorator.ts
@@ -1,59 +1,59 @@
 /**
- * method decorator
+ * method decorator - 함수, 메서드에 대해 얼마나 잘 알고 있는가?
  */
 class Idol {
-    name: string;
+  name: string;
 
-    constructor(name: string){
-        this.name = name;
-    }
+  constructor(name: string) {
+    this.name = name;
+  }
 
-    @TestMethodDecorator
-    @Configurable(false)
-    @MethodCallLogger('PROD')
-    dance(){
-        return `${this.name}가 춤을 춥니다.`;
-    }
+  @TestMethodDecorator
+  @Configurable(false)
+  @MethodCallLogger('PROD')
+  dance() {
+    return `${this.name}가 춤을 춥니다.`;
+  }
 }
 
 // target - static method에 데코레이팅을 할 경우 생성자 함수
 //        - instance method에 데코레이팅 할경우 인스턴스의 prototype
 // propertyKey - 메서드의 이름
 // descript - property descriptor
-function TestMethodDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor){
-    console.log('LogCall');
-    console.log('--- target ---');
-    console.log(target);
-    console.log('--- propertyKey ---');
-    console.log(propertyKey);
-    console.log('--- descriptor ---');
-    console.log(descriptor);
+function TestMethodDecorator(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  console.log('LogCall');
+  console.log('--- target ---');
+  console.log(target);
+  console.log('--- propertyKey ---');
+  console.log(propertyKey);
+  console.log('--- descriptor ---');
+  console.log(descriptor);
 }
 
 const rose = new Idol('로제');
 console.log('---- run dance ----');
 rose.dance();
 
-function Configurable(configurable: boolean){
-    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
-        descriptor.configurable = configurable;
-    }
+function Configurable(configurable: boolean) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    descriptor.configurable = configurable;
+  }
 }
 
 console.log(Object.getOwnPropertyDescriptors(Idol.prototype));
 
-function MethodCallLogger(env: string){
-    return function(target: any, propertyKey: string, descriptor: PropertyDescriptor){
-        const originalMethod = descriptor.value;
+function MethodCallLogger(env: string) {
+  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
 
-        descriptor.value = function(...args: any[]){
-            console.log(`[${env}] running function : ${propertyKey}`);
+    descriptor.value = function (...args: any[]) {
+      console.log(`[${env}] running function : ${propertyKey}`);
 
-            const result = originalMethod.apply(this, ...args);
+      const result = originalMethod.apply(this, ...args);
 
-            return result;
-        }
+      return result;
     }
+  }
 }
 
 console.log(rose.dance());
