c0210603eb19fd41921c8161364d42f9b6afb291
juju <55374275+smilk5u@users.noreply.github.com>
2024-08-28 21:22:40 +0900

    docs : 네임스페이스 및 export Default

--- 15_import_and_export/1_export_default_1.ts
+++ 15_import_and_export/1_export_default_1.ts
@@ -1,27 +1,28 @@
 /**
- * Export
+ * Export - namespace 대신에 사용하는 import 기능이다.
+ * ECMAsciprt 스탠다드에서 채택이 된 ECMAScript 모듈이라는 기능이다.
  */
 // export default 
-class IdolModel{
-    name: string;
-    age: number;
+class IdolModel {
+  name: string;
+  age: number;
 
-    constructor(name: string, age: number){
-        this.name = name;
-        this.age = age;
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
 const number = 12;
 
 // export default number;
 
-interface ICat{
-    name: string;
-    breed: string;
+interface ICat {
+  name: string;
+  breed: string;
 }
 
 export default {
-    IdolModel,
-    number,
+  IdolModel,
+  number,
 }
